47db5ca540890a18e0ff65064e013e43d2e53205
abzedmohammed <ibraabzed@gmail.com>
2023-02-13 14:50:41 +0300

    add loader

--- src/components/Dev.js
+++ src/components/Dev.js
@@ -11,10 +11,16 @@ export default function Dev({projects}) {
             <div className="col-md-1"></div> 
             <div className="col-md-10">
                 {
+                    projects ?
                     projects.map(project => {
                     return <Card key={project.id} title={project.title} picture={project.picture}
                     description={project.description} tags={project.tags} url={project.url} />
                 })
+                :
+                <div>
+                    <span className="loader"></span>
+                    <p className="text-center mt-3">Fetching projects from a very slow server😂😂😂. Please wait... </p>
+                </div>
                } 
             </div> 
             <div className="col-md-1"></div> 
